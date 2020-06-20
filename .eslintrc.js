9a9ba27e2d771bb28ad2fa5e648929c078b8bf25
Kyle Holmberg <kylemh.email@gmail.com>
2020-06-19 23:29:06 -0700

    chore: initial workspace wide setup
    Install deps and initial configs for TS, Prettier, ESLint, Husky, lint-staged, commitlint, lerna, stylelint, and yarn workspaces

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    commonjs: true,
+    es6: true,
+    node: true,
+  },
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+};
