02a4beb4ad08f3200ecbfea52f8bbdbc35d3279e
Kyle Holmberg <kylemh.email@gmail.com>
2020-07-15 15:19:35 -0700

    chore: fix typos

--- packages/zephyr/test/Button.test.tsx
+++ packages/zephyr/test/Button.test.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import { render, fireEvent } from '@testing-library/react';
 
-import * as Button from '../stories/Button.stories';
+import ButtonMeta, { Text as Button } from '../stories/Button.stories';
 
 describe('Button', () => {
   it('should fire callback onclick', () => {
     const onClick = jest.fn();
-    const { getByTestId } = render(<Button.WithRequiredProps onClick={onClick} />);
+    const { getByTestId } = render(<Button />);
 
     expect(onClick).not.toHaveBeenCalled();
-    fireEvent.click(getByTestId(Button.testID));
+    fireEvent.click(getByTestId(ButtonMeta.title));
     expect(onClick).toHaveBeenCalledTimes(1);
   });
 });
