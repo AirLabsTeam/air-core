f83efb1f8c56feabb626940d3a548083b7eff29a
Kyle Holmberg <kylemh.email@gmail.com>
2020-07-14 20:24:21 -0700

    chore: add jest config

---
+++ jest.config.js
@@ -0,0 +1,13 @@
+// For a detailed explanation regarding each configuration property, visit:
+// https://jestjs.io/docs/en/configuration.html
+module.exports = {
+  // NOTE: TSDX's jest config is merged into with this config like so:
+  // ...jestConfig
+  // where jestConfig = https://github.com/formium/tsdx/blob/master/src/index.ts#L512
+
+  // A list of reporter names that Jest uses when writing coverage reports
+  coverageReporters: ['json', 'lcov'],
+
+  // A list of paths to modules that run some code to configure or set up the testing framework before each test.
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+};
