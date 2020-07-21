b9cba8a59d06c8e3576d1ab72163b331a714402f
Kyle Holmberg <kylemh.email@gmail.com>
2020-07-21 16:09:59 -0700

    chore: try ignoring rule in CI again

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,4 +6,17 @@ const config = require('./packages/eslint-config/index');
 module.exports = {
   root: true,
   ...config,
+  overrides: [
+    ...config.overrides,
+    {
+      files: ['*'],
+      rules: {
+        /** Too many issues within a monorepo in CI
+         * @see https://github.com/benmosher/eslint-plugin-import/issues/1832
+         * @see https://github.com/benmosher/eslint-plugin-import/issues/1706
+         */
+        'import/no-unresolved': [!!process.env.CI ? 'off' : 'error'],
+      },
+    },
+  ],
 };
