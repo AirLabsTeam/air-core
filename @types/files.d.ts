fd29ccdaf31bfd912a86a95f15779ab2c8dd5f12
Kyle Holmberg <kylemh.email@gmail.com>
2020-07-24 18:13:41 -0700

    chore: add typing for .svg files

--- @types/files.d.ts
+++ @types/files.d.ts
@@ -2,3 +2,8 @@ declare module '*.mdx' {
   let MDXComponent: (props?: { [key: string]: any }) => JSX.Element;
   export default MDXComponent;
 }
+
+declare module '*.svg' {
+  const content: string;
+  export default content;
+}
