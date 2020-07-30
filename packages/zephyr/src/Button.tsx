0c51fddbe4ac826752df7193a07eaaac421e04fd
Kyle Holmberg <kylemh.email@gmail.com>
2020-07-30 07:50:13 -0700

    chore: adjust typing for lint error

--- packages/zephyr/src/Button.tsx
+++ packages/zephyr/src/Button.tsx
@@ -1,4 +1,4 @@
-import React, { MouseEvent as ReactMouseEvent, ButtonHTMLAttributes, FC } from 'react';
+import React, { MouseEvent as ReactMouseEvent, ButtonHTMLAttributes } from 'react';
 
 export interface ButtonProps
   extends Omit<ButtonHTMLAttributes<HTMLButtonElement>, 'disabled' | 'onClick'> {
@@ -6,13 +6,14 @@ export interface ButtonProps
   onClick: (event: ReactMouseEvent<HTMLButtonElement, MouseEvent>) => void;
 }
 
-export const Button: FC<ButtonProps> = (props) => {
+export const Button = (props: ButtonProps) => {
   return (
     <button
       {...{
         ...props,
         disabled: props.isDisabled,
         style: { ...props.style, padding: '1rem 0.5rem', backgroundColor: '#FAFAFA' },
+        type: props.type ?? 'button',
       }}
     />
   );
