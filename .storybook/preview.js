853bd6a608aeabce9495c2332a98a7e11668a5cb
Andy Tran <hello@andytran.me>
2020-08-29 15:52:03 -0700

    feat(storybook): fix theme  import path

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { ThemeProvider } from 'styled-components';
 
-import { theme } from '../packages/zephyr';
+import { theme } from '../packages/zephyr/src/theme';
 
 export const parameters = {
   actions: { argTypesRegex: '^on.*' },
