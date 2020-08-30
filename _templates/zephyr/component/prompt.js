d925c5c3c3bd91914717fad4921857ae800e0596
Andy Tran <hello@andytran.me>
2020-08-29 21:20:52 -0700

    feat(zepyhr): update hygen themeKey prompt message

--- _templates/zephyr/component/prompt.js
+++ _templates/zephyr/component/prompt.js
@@ -2,11 +2,11 @@ module.exports = [
   {
     type: 'input',
     name: 'name',
-    message: 'What is the component name? (ComponentName)',
+    message: 'What is the component name? (e.g. ComponentName)',
   },
   {
     type: 'input',
     name: 'themeKey',
-    message: 'What is the themeKey? (variants.componentKey)',
+    message: 'What is the component key in the theme object? (e.g. variants.componentKey)',
   },
 ];
