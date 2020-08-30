787b817790ddb43fa5e0a7a4ea9e97c5c3fa6a7c
Andy Tran <hello@andytran.me>
2020-08-29 20:20:13 -0700

    feat(zephyr): add in hygen template

---
+++ _templates/zephyr/component/prompt.js
@@ -0,0 +1,12 @@
+module.exports = [
+  {
+    type: 'input',
+    name: 'name',
+    message: 'What is the component name? (ComponentName)',
+  },
+  {
+    type: 'input',
+    name: 'themeKey',
+    message: 'What is the themeKey? (variants.componentKey)',
+  },
+];
