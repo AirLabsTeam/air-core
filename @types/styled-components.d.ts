5c5cbfe8dc9e067284142091e1b80c7cd3b0f89e
Andy Tran <hello@andytran.me>
2020-08-30 19:50:10 -0700

    fix(zephyr): remove styled components react types

--- @types/styled-components.d.ts
+++ @types/styled-components.d.ts
@@ -5,10 +5,3 @@ import { ThemeObject } from '../packages/zephyr';
 declare module 'styled-components' {
   export interface DefaultTheme extends ThemeObject {}
 }
-
-declare module 'react' {
-  interface Attributes {
-    /** @description use @styled-system/css */
-    css?: CssFunctionReturnType;
-  }
-}
