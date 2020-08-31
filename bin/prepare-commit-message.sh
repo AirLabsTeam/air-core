d0d523e4c9d5b3df604f1f39c27c7bde0954a3af
Kyle Holmberg <kylemh.email@gmail.com>
2020-08-30 21:50:42 -0700

    revert: undo faulty prepare commit msg changes

--- bin/prepare-commit-message.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# This ensures we dont use `cz-cli` on merge commits...
-# This didn't work https://stackoverflow.com/questions/56379208/ignoring-merge-commits-for-prepare-commit-msg-git-hook
-# But this did: https://stackoverflow.com/questions/61234845/how-to-bypass-pre-commit-hooks-after-fixing-merge-conflicts
-
-git rev-parse -q --verify MERGE_HEAD && false
