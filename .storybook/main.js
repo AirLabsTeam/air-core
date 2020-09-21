ce309d66c671416760ba690028b0baf72209cad8
Kyle Holmberg <kylemh.email@gmail.com>
2020-09-20 18:14:47 -0700

    chore: ensure stories are checked for type safety

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,7 @@
 module.exports = {
   stories: ['../packages/**/*.stories.{ts,tsx,js,jsx}'],
   addons: ['@storybook/addon-links', '@storybook/addon-actions', '@storybook/addon-essentials'],
+  typescript: {
+    check: true,
+  },
 };
