ed48eec95ee2622a4fe87d9e03139a4320c82270
Jessica Lin <jessicaa.lin@gmail.com>
2020-10-14 12:40:30 -0400

    feat: add grid view of all Icons to storybook

--- packages/icons/stories/Icons.stories.tsx
+++ packages/icons/stories/Icons.stories.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { Story, Meta } from '@storybook/react';
-import { Icon, IconProps } from '../src/index';
+import { Story, Meta, storiesOf } from '@storybook/react';
+import { Icon, IconProps, Icons, IconName } from '../src/index';
 
 const meta: Meta<IconProps> = {
   title: 'Icons/Icon',
@@ -20,3 +20,48 @@ const Template: Story<IconProps> = (args) => <Icon {...args} data-testid={meta.t
 
 export const Default = Template.bind({});
 Default.args = { name: 'Info' };
+
+const icons = Object.keys(Icons);
+
+storiesOf('Icons', module).add('All Icons (Grid View)', (args: IconProps) => {
+  return (
+    <div
+      style={{
+        display: 'flex',
+        flexWrap: 'wrap',
+        width: '100%',
+        justifyContent: 'flex-start',
+        background: '#f6f9fc',
+      }}
+    >
+      {icons.map((icon: IconName) => {
+        return (
+          <div style={{ display: 'grid', margin: '1.25rem' }} key={icon}>
+            <div
+              style={{
+                width: '100px',
+                height: '100px',
+                margin: '1.25rem',
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+              }}
+            >
+              <h6
+                style={{
+                  fontSize: '14px',
+                  fontFamily: 'sans-serif',
+                  margin: '0 0 0.75rem 0',
+                  textAlign: 'center',
+                }}
+              >
+                {icon}
+              </h6>
+              <Icon {...args} name={icon} />
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+});
