33c8d8ddfaa9cf0ecaf92d8fd42a677469c3d2d5
Jessica Lin <jessicaa.lin@gmail.com>
2020-10-23 12:57:15 -0400

    fix(icons): fix path for dist/icons.esm.js

--- packages/icons/stories/IndividualIcons.stories.tsx
+++ packages/icons/stories/IndividualIcons.stories.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 
-import * as ProductionBundleIconsMap from '../dist/icons.esm';
+// eslint-disable-next-line import/extensions
+import * as ProductionBundleIconsMap from '../dist/icons.esm.js';
 import data from '../src/IconPathData.json'; // used to get folder names and the stories in those folders
 
 const iconNames = Object.keys(ProductionBundleIconsMap);
