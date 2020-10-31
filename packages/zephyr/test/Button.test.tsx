7ed1319e372c1f423069551483bbbfaf362dcd6a
Andy Tran <hello@andytran.me>
2020-10-30 17:29:15 -0700

    fix(@air/zephyr): fix button import in test

--- packages/zephyr/test/Button.test.tsx
+++ packages/zephyr/test/Button.test.tsx
@@ -1,15 +1,21 @@
 import React from 'react';
 import { render, fireEvent } from '@testing-library/react';
 
-import ButtonMeta, * as Button from '../stories/Button.stories';
+import ButtonMeta, * as Button from '../stories/buttons/Button.stories';
 
 describe('Button', () => {
-  it('should fire callback onclick', () => {
+  it('should fire callback on click', () => {
     const onClick = jest.fn();
     const { getByTestId } = render(<Button.Default {...{ onClick }} />);
 
     expect(onClick).not.toHaveBeenCalled();
     fireEvent.click(getByTestId(ButtonMeta.title));
     expect(onClick).toHaveBeenCalledTimes(1);
+  });
+
+  it('should contain text', () => {
+    const { getByTestId } = render(<Button.Default>Button</Button.Default>);
+
+    expect(getByTestId(ButtonMeta.title)).toHaveTextContent('Button');
   });
 });
