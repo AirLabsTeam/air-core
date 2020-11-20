d0c7edb7b4bb93a800edd47d587b2a868f90a9f9
Jessica Lin <jessicaa.lin@gmail.com>
2020-11-20 12:51:50 -0500

    docs(icons): add instructions for icons

--- packages/icons/stories/Icons.stories.tsx
+++ packages/icons/stories/Icons.stories.tsx
@@ -19,7 +19,10 @@ export default {
     docs: {
       description: {
         component:
-          'Air icons are clear and simple. Each one of them sets the tone and language for our platform and help the user understand the concept it represents.',
+          "Icon svgs can now be imported directly inside a React component\
+          `import { Air as AirIcon } from '@air/icons';`\
+          Then you can use the icon as a component\
+          `<AirIcon tx={{ width: 65, color: 'jay500' }} />`  ",
       },
     },
   },
