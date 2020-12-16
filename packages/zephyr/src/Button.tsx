a64905189d448bb838921d1c0c540c251f0245f4
Jessica Lin <jessicaa.lin@gmail.com>
2020-12-15 21:37:19 -0500

    feat(zephyr/button): add loading state for filled buttons

--- packages/zephyr/src/Button.tsx
+++ packages/zephyr/src/Button.tsx
@@ -2,7 +2,10 @@ import React from 'react';
 import { transitions } from 'polished';
 import { forwardRefWithAs, PropsWithAs } from '@reach/utils';
 import { variant as styledSystemVariant } from 'styled-system';
-import { useTheme } from 'styled-components';
+import invariant from 'tiny-invariant';
+import { useReducedMotion } from 'framer-motion';
+import VisuallyHidden from '@reach/visually-hidden';
+import styled, { keyframes, useTheme } from 'styled-components';
 import { Box, BoxStylingProps } from './Box';
 import { ButtonVariantName } from './theme/variants/button';
 
@@ -14,9 +17,62 @@ export type ButtonSize = 'large' | 'medium' | 'small';
 export type NonSemanticButtonProps = Pick<BoxStylingProps, 'tx'> & {
   size?: ButtonSize;
   variant?: ButtonVariantName;
+  /**
+   * `isLoading` can only be true for buttons with filled variants.
+   */
+  isLoading?: boolean;
 };
 
 export interface ButtonProps extends PropsWithAs<'button', NonSemanticButtonProps> {}
+
+const Dot = () => (
+  <Box
+    tx={{
+      display: 'inline-block',
+      backgroundColor: 'currentColor',
+      borderRadius: 999,
+      height: 4,
+      width: 4,
+    }}
+  />
+);
+
+const wave = keyframes`
+  0% { transform: translate(0, -1.5px); }
+  50% { transform: translate(0, 1.5px); }
+  100% { transform: translate(0,-1.5px); }
+`;
+
+const Loader = styled(Box)<{ shouldReduceMotion: boolean }>`
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  display: flex;
+
+  div {
+    animation-name: ${({ shouldReduceMotion }) => (shouldReduceMotion ? 'none' : wave)};
+    animation-duration: 0.98s;
+    animation-timing-function: ease-in-out;
+    animation-delay: 0s;
+    animation-fill-mode: both;
+    animation-iteration-count: infinite;
+  }
+
+  div:nth-child(1) {
+    animation-delay: 0s;
+    margin-right: 4px;
+  }
+
+  div:nth-child(2) {
+    animation-delay: 0.14s;
+    margin-right: 4px;
+  }
+
+  div:nth-child(3) {
+    animation-delay: 0.28s;
+  }
+`;
 
 export const Button = forwardRefWithAs<NonSemanticButtonProps, 'button'>(
   (
@@ -26,19 +82,33 @@ export const Button = forwardRefWithAs<NonSemanticButtonProps, 'button'>(
       size = 'medium',
       type = 'button',
       variant = 'button-filled-blue',
+      isLoading = false,
       ref: _ref, // eslint-disable-line @typescript-eslint/no-unused-vars
+      children,
+      className = '',
       ...restOfProps
     }: ButtonProps,
     ref: React.Ref<HTMLButtonElement>,
   ) => {
+    invariant(
+      (isLoading &&
+        (variant === 'button-filled-blue' ||
+          variant === 'button-filled-grey' ||
+          variant === 'button-filled-destructive')) ||
+        !isLoading,
+      'Button can only have loading state if it is a filled variant',
+    );
+
     const theme = useTheme();
+    const shouldReduceMotion = useReducedMotion();
 
     return (
       <Box
         as={as}
-        disabled={disabled}
+        disabled={disabled || isLoading}
         type={type}
         variant={variant}
+        className={isLoading ? `${className} isLoading` : `${className}`}
         __baseStyles={{
           appearance: 'none',
           outline: 'none',
@@ -87,7 +157,28 @@ export const Button = forwardRefWithAs<NonSemanticButtonProps, 'button'>(
         }}
         {...restOfProps}
         ref={ref}
-      />
+      >
+        <Box role="status" tx={{ position: 'relative' }}>
+          {isLoading && (
+            <>
+              <VisuallyHidden>Loading...</VisuallyHidden>
+              <Loader aria-hidden="true" shouldReduceMotion={shouldReduceMotion!}>
+                <Dot />
+                <Dot />
+                <Dot />
+              </Loader>
+            </>
+          )}
+          <Box
+            tx={{
+              opacity: isLoading ? 0 : 1,
+              visibility: isLoading ? 'hidden' : 'visible',
+            }}
+          >
+            {children}
+          </Box>
+        </Box>
+      </Box>
     );
   },
 );
