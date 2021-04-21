1e38537b98b20733f950ec6f29a75919201528fa
Andy Tran <hello@andytran.me>
2021-04-21 11:14:56 -0700

    feat(@air/zephyr): add in menu state component to give access to the menu state

--- packages/zephyr/stories/menus/abstractions/DropdownMenu.stories.tsx
+++ packages/zephyr/stories/menus/abstractions/DropdownMenu.stories.tsx
@@ -240,3 +240,44 @@ withChildrenBottom.parameters = {
     },
   },
 };
+
+export const withChangeEvent = Template.bind({});
+
+withChangeEvent.args = {
+  onChange: (isExpanded) => console.log('Is the menu opened?', isExpanded),
+  options: [
+    {
+      leftAdornment: <Box as={LinkIcon} tx={{ display: 'block', width: 16 }} />,
+      label: 'Share a link',
+      description: 'Generate a share link for public or private use',
+      onSelect: () => {},
+    },
+    {
+      leftAdornment: <Box as={AddMemberIcon} tx={{ display: 'block', width: 16 }} />,
+      label: 'Add members',
+      description: 'Invite members or guests to collaborate on this board',
+      hasDivider: true,
+      onSelect: () => {},
+    },
+    {
+      description: 'Noooooooooooooooooo, please don’t delete me 😢',
+      leftAdornment: (
+        <Box as={LinkIcon} tx={{ display: 'block', width: 16, color: 'flamingo500' }} />
+      ),
+      label: 'Delete',
+      onSelect: () => window.alert('You actually tried to....'),
+      tx: {
+        color: 'flamingo800',
+      },
+    },
+  ],
+  trigger: <Button variant="button-filled-blue">Options</Button>,
+};
+
+withChangeEvent.parameters = {
+  docs: {
+    description: {
+      story: 'The `onChange` prop returns the state of the menu everytime it is opened and closed.',
+    },
+  },
+};
