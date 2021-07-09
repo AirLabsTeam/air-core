33b6aff866799f6a74f094602369a1e1de18e096
Andy Tran <hello@andytran.me>
2021-07-09 14:01:31 -0700

    feat(@air/zephyr): add in button link

--- packages/zephyr/src/theme/variants/index.ts
+++ packages/zephyr/src/theme/variants/index.ts
@@ -1,16 +1,19 @@
 import { button } from './button';
+import { buttonLink } from './button-link';
 import { field } from './field';
 import { modal } from './modal';
 import { text } from './text';
 
 export const variants = {
   ...button,
+  ...buttonLink,
   ...field,
   ...modal,
   ...text,
 };
 
 export * from './button';
+export * from './button-link';
 export * from './field';
 export * from './modal';
 export * from './text';
