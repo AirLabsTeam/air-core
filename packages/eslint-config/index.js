6610e33543a074f5b62354653889c31db8b29ab3
Kyle Holmberg <kylemh.email@gmail.com>
2021-07-14 02:09:44 -0700

    chore: adjust lint rules for react 17

--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -107,8 +107,13 @@ const config = {
      * React Plugin Rules
      * @see https://github.com/yannickcr/eslint-plugin-react#list-of-supported-rules
      * */
-    'react/jsx-uses-react': 'error',
-    'react/jsx-uses-vars': 'error',
+    /**
+     * Not needed after React 17
+     * @see https://reactjs.org/blog/2020/09/22/introducing-the-new-jsx-transform.html#eslint
+     */
+    'react/jsx-uses-react': 'off',
+    'react/jsx-uses-vars': 'off',
+    'react/react-in-jsx-scope': 'off',
 
     /**
      * Import Plugin Rules
@@ -364,7 +369,6 @@ const config = {
       // assuming Next.js application
       files: '**/pages/**/*.tsx',
       rules: {
-        'react/react-in-jsx-scope': 'off', // react is a global in this folder
         'import/no-default-export': 'off', // pages have to have a default export
       },
     },
