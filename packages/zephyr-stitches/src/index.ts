468b4dd267e8e51744289393f94825cfb0b15471
Andy Tran <hello@andytran.me>
2021-11-30 10:38:36 -0800

    fix(@air/zephyr-stitches): export all components

--- packages/zephyr-stitches/src/index.ts
+++ packages/zephyr-stitches/src/index.ts
@@ -2,4 +2,9 @@ export * from './stitches.config';
 
 export * from './Box';
 export * from './Button';
+export * from './IconButton';
+export * from './Pill';
+export * from './Tag';
+export * from './Text';
+export * from './Tooltip';
 export * from './VisuallyHidden';
