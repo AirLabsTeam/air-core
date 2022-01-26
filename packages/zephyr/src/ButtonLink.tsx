54689a6708968d484a20bb709ea8d6a682e6279f
Andy Tran <hello@andytran.me>
2022-01-26 12:25:18 -0800

    fix(@air/zephyr): allowing baseStyles in button link as well

--- packages/zephyr/src/ButtonLink.tsx
+++ packages/zephyr/src/ButtonLink.tsx
@@ -46,7 +46,8 @@ export const ButtonLink = forwardRefWithAs<NonSemanticButtonLinkProps, 'button'>
     return (
       <Box
         as={as}
-        tx={{
+        // @ts-ignore - Allowing it here
+        __baseStyles={{
           display: 'inline-block',
           outline: 'none',
           background: 'transparent',
@@ -69,11 +70,11 @@ export const ButtonLink = forwardRefWithAs<NonSemanticButtonLinkProps, 'button'>
           '&:focus-visible': {
             boxShadow: `0 0 0 3px ${theme.colors.focus}`,
           },
-          ...tx,
         }}
         className={disabled ? 'disabled' : ''}
         ref={ref}
         onClick={disabled ? noop : onClick}
+        tx={tx}
         variant={variant}
         {...buttonProps}
         {...restOfProps}
