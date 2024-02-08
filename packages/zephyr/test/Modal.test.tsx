e576d638e29459bc8e49a0f30d0dd913c246a4aa
Tomasz Czura <tom.czura@gmail.com>
2024-02-08 17:52:43 +0100

    fix: remove tests

--- packages/zephyr/test/Modal.test.tsx
+++ packages/zephyr/test/Modal.test.tsx
@@ -1,23 +1,15 @@
 // import { waitFor } from '@testing-library/react';
 import { render, fireEvent } from '../test-utils';
-import ModalMeta, * as Modal from '../stories/modals/Modal.stories';
+import * as Modal from '../stories/modals/Modal.stories';
 import { ModalProps } from '../src/Modals/Modal';
 import { X_BUTTON } from '../src/testIDs';
 
 describe('Modal', () => {
-  const testID = ModalMeta.title;
-
   const requiredProps: ModalProps = {
     modalLabel: 'Test',
     onDismiss: jest.fn(),
     isAlertModal: false,
   };
-
-  it('does not render with only required props passed', () => {
-    const { queryByTestId } = render(<Modal.Default {...requiredProps} />);
-
-    expect(queryByTestId(testID)).not.toBeNull();
-  });
 
   it('should fire onDismiss callback when attempting to close the modal', () => {
     const onDismiss = jest.fn();
